d197f8c30675fa43d08130ed52ffa271c38fffcf
Tomasz Woźny <63068055+Dziameno@users.noreply.github.com>
2021-06-10 14:35:27 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -20,7 +20,7 @@ $row = mysqli_fetch_array($update)
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <title>Strona Główna</title>
+    <title>Edytowanie wpisu</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="styles.css" type="text/css"/>
 </head>
