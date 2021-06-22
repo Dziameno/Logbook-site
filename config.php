eae8e964239d0f219c7d8033d02d37662b749676
Tomasz Woźny <63068055+Dziameno@users.noreply.github.com>
2021-06-22 12:24:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-west-01.cleardb.com');
-define('DB_USERNAME', 'b13fd7b0a07e94');
-define('DB_PASSWORD', '3ac53242');
-define('DB_NAME', 'heroku_c856f6eb5222ec9');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
